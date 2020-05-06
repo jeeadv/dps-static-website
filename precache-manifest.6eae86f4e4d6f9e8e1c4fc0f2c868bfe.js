b4f15623b1a9111552022484bac7b0361f9ab4da
Kumar Gaurav (Delivery) <KUMAG@amdocs.com>
2020-05-06 20:32:31 +0530

    Updates

---
+++ precache-manifest.6eae86f4e4d6f9e8e1c4fc0f2c868bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743c73c7a62833bfbfe22de7b09d1120",
+    "url": "/dps-static-website/index.html"
+  },
+  {
+    "revision": "1fb3974ce0edcdc5e079",
+    "url": "/dps-static-website/static/css/main.21b38ea8.chunk.css"
+  },
+  {
+    "revision": "83e6893b756810f618e4",
+    "url": "/dps-static-website/static/js/2.d1ff8164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dps-static-website/static/js/2.d1ff8164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb3974ce0edcdc5e079",
+    "url": "/dps-static-website/static/js/main.4f41fafd.chunk.js"
+  },
+  {
+    "revision": "b5fe70ef45d42043263c",
+    "url": "/dps-static-website/static/js/runtime-main.cd509f97.js"
+  },
+  {
+    "revision": "6e61426d26433cefbbdf1e676a970af6",
+    "url": "/dps-static-website/static/media/principalImage.6e61426d.jpg"
+  }
+]);
